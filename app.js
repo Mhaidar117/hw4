af050a0b7172eefc4c53e73529f2fdc05a06e09b
Michael Haidar <mhaidar@csumb.edu>
2019-10-30 12:57:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/", function(req, res){
+   res.render("index.html");
+
+});
+app.get("/index", function(req, res){
+    res.render("index.html");
+});
+
+app.get("/supervised", function(req, res){
+    res.render("supervised.html");
+});
+
+app.get("/unsupervised", function(req, res){
+    res.render("unsupervised.html");
+});
+
+app.get("/online", function(req, res){
+    res.render("online.html");
+});
+
+app.get("/batch", function(req, res){
+    res.render("batch.html");
+});
+
+//server listener
+app.listen("8081", "127.0.0.1", function(){
+   console.log("Express Server is Running...");
+
+});
